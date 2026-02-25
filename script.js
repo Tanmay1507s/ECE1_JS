5e783fba3b2686dd0b2d7e150467bfcb91ca80d8
Tanmay <tanmays8899@gmail.com>
2026-02-25 09:53:25 +0530

    primitive type

---
+++ script.js
@@ -0,0 +1,17 @@
+console.log("Hello World")
+var num ;
+num = 10;
+console.log(num);
+console.log(typeof(num));
+num = false;
+console.log(num);
+comsole.log(typeof(num));
+num = "ten";
+console.log(num);
+console.log(typeof(num));
+num = null;
+console.log(num);
+console.log(typeof(num));
+num = undefined;
+console.log(num);
+console.log(typeof(num));
